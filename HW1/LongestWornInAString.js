ff6871f9cbddd29123c4e528f0eadbee2acf9c03
Yaroslav Kozak <yaroslav.kozak@techfabric.co>
2021-08-30 15:45:16 +0300

    Added hw1, and basic app, created via .net react

---
+++ HW1/LongestWornInAString.js
@@ -0,0 +1,9 @@
+function findLongestWordLength(str) {
+  let longestWord = str.split(" ").reduce(function(longest, currentWord) {
+    return currentWord.length > longest.length ? currentWord : longest;
+  }, "");
+
+  return longestWord.length;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
